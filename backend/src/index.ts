e3f024c81258f6e76629fe8ddacc606191e5cd42
singhvibhanshu <singhvibhanshu@hotmail.com>
2025-08-01 23:03:31 +0530

    added the jwt logic for hono. that's it for today guys. gn

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { use } from 'hono/jsx'
+import { sign } from 'hono/jwt'
+import { jwk } from 'hono/jwk'
 
 const app = new Hono<{
   Bindings: {
@@ -16,14 +17,18 @@ app.post('/api/v1/signup', async (c) => {
 
   const body = await c.req.json()
 
-  await prisma.user.create({
+  const user = await prisma.user.create({
     data: {
       email: body.email,
       password: body.password,
     },
   })
 
-  return c.text('Hello Hono!')
+  const token = sign({ id: user.id }, "secret")
+
+  return c.json({
+    jwt: token
+  })
 })
 
 app.post('/api/v1/signin', (c) => {
