2a47b648387930d9ea6b801ba3b41bb57a1951a2
singhvibhanshu <singhvibhanshu@hotmail.com>
2025-08-02 20:30:58 +0530

    added remaining route handlers logic

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,4 +1,24 @@
 import { Hono } from "hono";
 
-export const blogRouter = new Hono();
+export const blogRouter = new Hono<{
+    Bindings: {
+      DATABASE_URL: string,
+      JWT_SECRET: string
+    }
+}>();
 
+blogRouter.post('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+blogRouter.put('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+blogRouter.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+blogRouter.get('/bulk', (c) => {
+  return c.text('Hello Hono!')
+})
