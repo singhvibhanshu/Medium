c8d52a28069d1e4a8324a89493af3fed9848cefa
singhvibhanshu <singhvibhanshu@hotmail.com>
2025-08-02 17:39:41 +0530

    empty blog.ts and user.ts file added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,6 +10,9 @@ const app = new Hono<{
   }
 }>()
 
+app.route('/api/v1/user', userRouter);
+app.route('/api/v1/blog', blogRouter);
+
 app.use('/api/v1/blog/*', async (c, next) => {
   const header =  c.req.header("authorization")|| "";
   const token = header.split(" ")[1]
