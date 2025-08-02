d0f3992b54042f9c505ab10d26a7ce5964d0814c
singhvibhanshu <singhvibhanshu@hotmail.com>
2025-08-02 11:43:08 +0530

    user authorization logic added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,22 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { sign } from 'hono/jwt'
+import { sign, verify } from 'hono/jwt'
 
 const app = new Hono()
 
 app.use('/api/v1/blog/*', async (c, next) => {
-  await next();
+  const header =  c.req.header("authorization")|| "";
+  const token = header.split(" ")[1]
+
+  //@ts-ignore
+  const response = await verify(header, c.env.JWT_SECRET)
+  if (response.id) {
+    next()
+  } else {
+    c.status(403)
+    return c.json({ error: "unauthorized" });
+  }
 })
 
 app.post('/api/v1/signup', async (c) => {
